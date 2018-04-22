1d06238bc2723e33b9515060dd96c7eb8ffba102
Thomas <thomas.wangsa@amarbank.co.id>
2018-04-22 15:41:02 +0700

    Init Project

---
+++ app/Http/Controllers/Home/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    protected function home() {
+    	echo "HOME";die;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('home');
+});
+
+
+// Route::get('/home', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/'              , 'Home\HomeController@home');
+Route::get('auth/login'     , 'Auth\LoginController@showLoginForm')->name('login');
+
+
+Route::group(['middleware' => ['auth']], function()
+{   
+ 		
+    Route::get('home', 'Home\HomeController@home');
+    //Route::get('auth/logout', function () {return redirect('auth/login');});
+        
+});
