bdb71ab745d3e8b582c242754ad310b6e633a7c3
thomas.wangsa@finaccel.co <email@example.com>
2019-07-02 06:02:37 +0700

    add meta

--- resources/views/layouts/map.blade.php
+++ resources/views/layouts/map.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="author" content="thomas.wangsa@gmail.com">
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="author" content="thomas.wangsa@gmail.com">
     
 
     <!-- Styles -->
